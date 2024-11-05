ba103139d886296dc198a1dbfed984da416ebd7c
Captain-Fahd <omarfahd06@gmail.com>
2024-11-05 22:34:10 +1100

    Added basic logic for the addition of the book. Added basic HTML layout.
    Added dialog for HTML, along with the basic inputs. Added JS logic for the addition of the books.

---
+++ script.js
@@ -0,0 +1,42 @@
+const myLibrary = [];
+
+let dialog = document.querySelector(".new-book");
+let nameInput = document.querySelector(".name-input");
+let pagesInput = document.querySelector(".pages-input");
+let statusInput = document.querySelector(".status-input");
+let dialogButton = document.querySelector(".create-book")
+let bookList = document.querySelector(".book-list");
+let bookSubmit = document.querySelector(".submit-book");
+
+function Book(name, numPage, status) {
+  this.name = name,
+  this.numPage = numPage,
+  this.status = status
+}
+
+function addBookToLibrary(name, numPage, status) {
+  let book = name + "/" + numPage + "/" + status;
+  return myLibrary.push(book);
+}
+
+function displayBook() {
+  for(let i = 0; i < myLibrary.length; i++){
+    let newBook = document.createElement("li")
+    newBook.textContent = myLibrary[i];
+    bookList.appendChild(newBook);
+  }
+}
+
+dialogButton.addEventListener('click', () => {
+  dialog.showModal();
+})
+bookSubmit.addEventListener('click', () => {
+  let bookInput = nameInput.value;
+  let pageNums = pagesInput.value;
+  let bookStatus = statusInput.value;
+
+  let currentBook = new Book(bookInput, pageNums, bookStatus);
+  addBookToLibrary(currentBook);
+  displayBook();
+  dialog.close();
+})
